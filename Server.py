dfb1efa81fe22609b0af061b5ad8c6cd3165ef34
DiegoMC9 <dmartinezcapel@gmail.com>
2018-07-22 14:32:46 +0200

    arreglos

--- Server.py
+++ Server.py
@@ -11,7 +11,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
 server_address = ('', 80)
 print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+try:
+    sock.bind(server_address)
+except socket.error as msg:
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
 IP=socket.gethostbyname(socket.gethostname())
 msg = ""
 
